6f782a88355322b4afd9d62394e414efa9f384a0
Yuanzhuoxin <1974269607@qq.com>
2025-07-19 13:09:26 +0800

    chore: 样式名称

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,5 +1,7 @@
-/** 密接容器 */
-export const fullContainerClass = 'w-full h-full relative overflow-hidden m-0 border-0 p-0'
+/** 占据父元素全部剩余空间 相对定位 不滚动 */
+const autoFillClassName = 'flex-1 w-full h-full relative overflow-hidden'
+export { autoFillClassName as AutoFill }
 
 /** 基于absolute定位的居中 */
-export const absoluteCenterClass = 'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'
+const absoluteCenterClassName = 'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'
+export { absoluteCenterClassName as AbsoluteCenter }
