7be057e34945c208ed294c5f27b9be4e713cd6ad
Fane <141594291+FanetheDivine@users.noreply.github.com>
2025-10-23 09:36:25 +0800

    chore: 类型和注释 (#5)

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,10 +1,11 @@
 export {}
 
 declare global {
+  /** className和style */
   type Style = {
     className?: string
     style?: React.CSSProperties
   }
+  /** 判断一个类型是否可能是函数 */
   type isFunction<T> = T extends (...args: any[]) => any ? true : false
-  type NotFunction<T> = isFunction<T> extends true ? never : T
 }
