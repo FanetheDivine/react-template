f0ca61d50f6bb3287a4ac5820aa39921226beaf5
FanetheDivine <1974269607@qq.com>
2025-11-02 12:26:08 +0800

    build分析

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,27 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import { defineConfig } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
+import { defineConfig, PluginOption } from 'vite'
 import fsRouter from './vite-fs-router-plugin/index.js'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(), fsRouter()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig((options) => {
+  const plugins: PluginOption[] = [react(), tailwindcss(), fsRouter()]
+  if (options.mode === 'analyze') {
+    plugins.push(
+      visualizer({ filename: './dist/stats.html', gzipSize: true, brotliSize: true, open: true }),
+    )
+  }
+  return {
+    plugins,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  server: {
-    host: '0.0.0.0',
-  },
+    server: {
+      host: '0.0.0.0',
+    },
+  }
 })
