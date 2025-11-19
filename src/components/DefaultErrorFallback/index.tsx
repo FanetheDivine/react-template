0c912e9b53497ddd01fa0a84891849bd3d7386ee
Fane <godwokenfane@gmail.com>
2025-11-19 17:51:32 +0800

    default fallback

--- src/components/DefaultErrorFallback/index.tsx
+++ src/components/DefaultErrorFallback/index.tsx
@@ -8,23 +8,24 @@ export const DefaultErrorFallback: FC<FallbackProps> = (props) => {
   const { error, resetErrorBoundary } = props
   const { t } = useTranslation()
   return (
-    <Result
-      className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-4/5'
-      status={'error'}
-      title={t('common.pageError')}
-      subTitle={error?.message}
-      extra={[
-        <Button key='retry' type='primary' icon={<ReloadOutlined />} onClick={resetErrorBoundary}>
-          {t('common.retry')}
-        </Button>,
-        <Button
-          key='refresh'
-          // eslint-disable-next-line no-self-assign
-          onClick={() => (window.location.href = window.location.href)}
-        >
-          {t('common.reloadPage')}
-        </Button>,
-      ]}
-    />
+    <div className='flex h-full w-full flex-1 items-center justify-center'>
+      <Result
+        status={'error'}
+        title={t('common.pageError')}
+        subTitle={error?.message}
+        extra={[
+          <Button key='retry' type='primary' icon={<ReloadOutlined />} onClick={resetErrorBoundary}>
+            {t('common.retry')}
+          </Button>,
+          <Button
+            key='refresh'
+            // eslint-disable-next-line no-self-assign
+            onClick={() => (window.location.href = window.location.href)}
+          >
+            {t('common.reloadPage')}
+          </Button>,
+        ]}
+      />
+    </div>
   )
 }
