0728f5b58ec16b699a962119c88c54f580c14f38
Fane <godwokenfane@gmail.com>
2025-11-20 10:24:08 +0000

    样式

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -4,7 +4,7 @@ const Layout: FC<PropsWithChildren> = (props) => {
   return (
     <div className={'flex h-full w-full flex-col'}>
       <div className='h-20 bg-black text-white'>layout</div>
-      <div className={'mt-20 flex-1'}>{props.children}</div>
+      <div className={'mt-20 flex-1 overflow-auto'}>{props.children}</div>
     </div>
   )
 }
